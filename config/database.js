d472a0319ffbd2fedb617ca82a91a93be496ec96
Alex.ZIRNHELT <alexis.zirnhelt@gmail.com>
2026-03-04 08:38:13 +0100

    improve: optimisation du démarrage du serveur

--- config/database.js
+++ config/database.js
@@ -12,12 +12,8 @@ const sequelize = new Sequelize(
 );
 
 const testConnexion = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connexion à la base de données réussie.');
-  } catch (error) {
-    console.error('Impossible de se connecter à la base de données :', error);
-  }
+  await sequelize.authenticate();
+  console.log('Connexion à la base de données réussie.');
 };
 
 module.exports = { sequelize, testConnexion };
